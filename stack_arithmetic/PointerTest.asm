718406d0edbb70a16da2c6838af37ebf74b66c31
Lucas Ferreira <75485471+ferris77@users.noreply.github.com>
2021-03-21 23:02:22 -0300

    Add files via upload

---
+++ stack_arithmetic/PointerTest.asm
@@ -0,0 +1,128 @@
+
+//push constant 3030
+@3030
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+//pop pointer 0
+@SP
+M=M-1
+A=M
+D=M
+@3
+M=D
+
+//push constant 3040
+@3040
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+//pop pointer 1
+@SP
+M=M-1
+A=M
+D=M
+@4
+M=D
+
+//push constant 32
+@32
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+//pop this 2
+@SP
+M=M-1
+A=M
+D=M
+@3032
+M=D
+
+//push constant 46
+@46
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+//pop that 6
+@SP
+M=M-1
+A=M
+D=M
+@3046
+M=D
+
+//push pointer 0
+@3
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+//push pointer 1
+@4
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+//add
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+M=D+M
+
+//push this 2
+@3032
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+//sub
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+M=M-D
+
+//push that 6
+@3046
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+//add
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+M=D+M
